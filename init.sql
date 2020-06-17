b04f1f0e9e1dc1a8f41089d4ab264153ce2bac0c
akai0211 <duythu021194@gmail.com>
2020-06-17 12:54:40 +0700

    Update for testing

--- init.sql
+++ init.sql
@@ -6,5 +6,3 @@ CREATE TABLE customer
     email character varying(200) NOT NULL,
     phone character varying(20) NOT NULL
 );
-INSERT INTO customer (id, name, address, email, phone) VALUES (1, 'Thu Bui', '12 Main Street', 'test@test.com', '511-812-5233');
-INSERT INTO customer (id, name, address, email, phone) VALUES (2, 'Test', '12 Main Street', 'test2@test.com', '511-812-5233');
